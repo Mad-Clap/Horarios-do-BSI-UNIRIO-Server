c16dfbc9c952f309afb2630d1d1c1fa33550660d
Mad-Clap <leolima345995@gmail.com>
2026-03-23 19:01:29 -0300

    primeiro commit

---
+++ index.ts
@@ -0,0 +1,121 @@
+import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
+import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
+import type { CallToolResult } from '@modelcontextprotocol/sdk/spec.types.js';
+import { extrairHorariosBsi } from "./lib";
+import z4 from 'zod/v4';
+import { CreateMessageResultSchema } from '@modelcontextprotocol/sdk/types.js';
+import type { RequestHandlerExtra } from '@modelcontextprotocol/sdk/shared/protocol.js';
+
+
+
+let horarioBsiJSON: Record<string, object> = await extrairHorariosBsi()
+    .catch(error => {
+        console.error('Server error:', error.cause);
+        process.exit(1);
+    })
+;
+
+// Criando o servidor MCP
+const app = new McpServer({
+        name: "Horarios do BSI UNIRIO Server",
+        version: "1.0.0",
+    },
+);
+
+/*
+app.registerResource(
+    'horarioBSI-resource',
+    "memoria://horarios-BSI",
+    {
+        title: 'horarios do curso de Bacharelado de Sistemas de Informação (BSI) da universidade UNIRIO',
+        description: 'Planilha convertida em JSON com horarios do curso de Bacharelado de Sistemas de Informação (BSI) da universidade UNIRIO',
+        mimeType: 'application/json'
+    },
+    async ():Promise<ReadResourceResult> =>{
+        return{
+            contents: [
+                {
+                    uri: "memoria://horarios-BSI",
+                    text: JSON.stringify(horarioBsiJSON)
+                }
+            ]
+        }
+    }
+)
+*/
+
+
+app.registerTool(
+    'Horarios_BSI',
+    {
+        title: 'horarios do curso de Bacharelado de Sistemas de Informação da universidade UNIRIO',
+        description: 'Planilha convertida em json dos horarios do curso de Bacharelado de Sistemas de Informação da universidade UNIRIO'
+    },
+    async (): Promise<CallToolResult> => {
+        
+        return { content: [{ type: "text", text: JSON.stringify(horarioBsiJSON) }] };
+    }
+);
+
+
+app.registerTool(
+    'Tabela_de_materias_BSI',
+    {
+        title: 'horarios das matérias do curso de Bacharelado de Sistemas de Informação da universidade UNIRIO que estão em curso pelo usuário',
+        description: 'ferramenta para auxílio na geração de imagem de tabela dos horarios do curso de BSI das matérias definidas pelo usuário',
+        inputSchema: {
+        materias: z4.string().array()
+        .describe("matérias definidas pelo usuário do curso de BSI"),
+    },
+    },
+    async ({ materias }, extra: RequestHandlerExtra<any, any>) => {
+        
+       const response = await extra.sendRequest(
+        {
+          method: "sampling/createMessage",
+          params: {
+            messages: [
+              {
+                role: "user",
+                content: {
+                  type: "text",
+                  text: `por favor crie e salve uma tabela em html com as matérias de Bacharelado de
+                  Sistemas de Informação da UNIRIO listadas a seguir: ${materias}.
+                  A tabela deve ser construída da seguinte forma: a primeira linha deve ser os dias da semana,
+                   e a primeira coluna deve ser os horários das matérias. Deve ser registrado na tabela o nome por extenso das matérias 
+                   (exemplo: "técnicas de programação", "Arquitetura de Computadores"),
+                    o nome da turma em que ela será ministrada (geralmente o nome simplificado sem vogais, como "ALGPROG", "ARQCOMP"),
+                    e a sala em que a aula ocorrerá. A cor das células da primeira linha deve ser azul, e a cor das células com os horários também devem ser azul. 
+                    As demais linhas devem alternar entre verde claro e branco. As informações nas células devem ficar na cor preta. 
+                    Utilize as informações contidas nesta planilha do horários de BSI convertida em JSON para encontrar as informações sobre as matérias: ${JSON.stringify(horarioBsiJSON)}`,
+                },
+              },
+            ],
+            maxTokens: 200
+          },
+        },
+        CreateMessageResultSchema
+      );
+
+        return {
+            content: [
+                {
+                    type: 'text',
+                    text:  response.content.type === 'text' ? response.content.text : 'não foi possível printar a tabela'
+                }
+            ]
+        };
+    }
+);
+
+
+//inicializando o servidor MCP
+async function main() {
+    const transport = new StdioServerTransport();
+    await app.connect(transport);
+}
+
+main().catch(error => {
+    console.error('Server error:', error);
+    process.exit(1);
+});
---
+++ lib.ts
@@ -0,0 +1,63 @@
+import puppeteer from 'puppeteer-core'
+import xlsx from "xlsx";
+import os from 'node:os'
+import path from 'node:path';
+
+export async function extrairHorariosBsi(): Promise<Record<string, object>> {
+    
+    const horarioBsi: Bun.BunFile = await baixarPlanilhaBSI();
+
+    let workbook = xlsx.readFile(horarioBsi.name!)
+    let horariosJson: Record<string, object> = {};
+    
+    workbook.SheetNames.forEach(name => {
+    
+        let rangeNumber: number = name.includes("Grade")? 2 : 1;
+        let aux = xlsx.utils.sheet_to_json(workbook.Sheets[name]!, {range : rangeNumber });
+        horariosJson[name] = aux
+    })
+
+    return horariosJson;
+}
+
+
+async function baixarPlanilhaBSI(): Promise<Bun.BunFile>{
+
+    const user = os.homedir();
+    const pathPlanilhaBsi = path.join(user, 'Horarios-BSI.xlsx')
+    let planilhaBsi = Bun.file(pathPlanilhaBsi,
+        {type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"})
+
+    try {
+        const browser = await puppeteer.launch({channel:'chrome'});
+        const page = await browser.newPage();
+
+        await page.goto('https://bsi.uniriotec.br/horarios-curriculo-novo/')
+
+        const iframeSelector = 'iframe.iframe-class[src*="://docs.google.com/spreadsheets"]';
+        const iframeSrc: string = await page.$eval(iframeSelector, el => el.src);
+        let spreadsheetId: string = iframeSrc.match("d\/.+\/")![0];
+
+        const downloadUrl = `https://docs.google.com/spreadsheets/${spreadsheetId}export?format=xlsx`;
+        const response = await fetch(downloadUrl);
+
+        await Bun.write(planilhaBsi, response);
+
+        browser.close();
+
+        
+    } catch (error) {
+
+        let fileExist: Boolean  = await planilhaBsi.exists();
+        if(!fileExist){
+            console.error(planilhaBsi)
+            throw new Error(
+                "impossível acessar planilha com horários do BSI", 
+                {cause: "Não foi possível acessar o site de BSI para extrair a planilha atualizada, e não há uma versão salva no servidor"}
+            );
+        } 
+            
+    }
+
+    return planilhaBsi;
+}
